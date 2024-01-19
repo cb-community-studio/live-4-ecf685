11766bbeea06ab4e56af10e883fa0403ad4c0d9e
cb-machine-account <115591081+cb-machine-account@users.noreply.github.com>
2024-01-19 08:30:43 +0800

    Initial commit

--- nodejs-backend/src/services/topics/topics.class.js
+++
@@ -1,5 +0,0 @@
-const { Service } = require('feathers-mongoose');
-
-exports.Topics = class Topics extends Service {
-  
-};
--- nodejs-backend/src/services/userdetails/userdetails.class.js
+++
@@ -1,5 +0,0 @@
-const { Service } = require('feathers-mongoose');
-
-exports.Userdetails = class Userdetails extends Service {
-  
-};
